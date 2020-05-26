ef082c8252c6ed2b67fa200f950f1d1e840c6afa
manhnguyen0502 <nguyentrongmanh050298@gmail.com>
2020-05-26 20:41:42 +0700

    init home page

---
+++ resources/views/layouts/common/footer.blade.php
@@ -0,0 +1,10 @@
+ <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; 2020 Training Studio
+                - Designed by <a rel="nofollow" href="https://templatemo.com" class="tm-text-link" target="_parent">TemplateMo</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
